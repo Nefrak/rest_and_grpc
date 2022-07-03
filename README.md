3e076742e73b4e21b28316be32163eeab17ef9ef
Nefrak <Jara.Franek@gmail.com>
2022-07-03 17:35:06 +0200

    sql done

--- README.md
+++ README.md
@@ -1,1 +1,56 @@
-# task_rest_python
+# Aplikace a script podle zadání
+- Python 3.10.4
+- psql (14.4 (Ubuntu 14.4-1.pgdg22.04+1))
+
+## Obsah (struktura repozitáře)
+- python
+    - app
+        - __init__.py
+        - ArgParser.py - obsahuje třídu parsující argumenty (modul)
+        - file-client.py - klientská aplikace
+        - grpc_server.py - jednoduchý server pro základní testování
+        - rest_server.py - jednoduchý server pro základní testování
+        - service_file_pb2_grpc.py - specifikace pro grpc server
+        - service_file_pb2.py - specifikace pro grpc server
+        - test_file-client.py - unit testy
+        - test.txt - soubor jehož obsah je vracen rest serverem.
+    - README.rts - popis zadané aplikace
+    - rest_file.rts - popis rest api
+    - service_file.proto - popis grpc
+ - sql
+    - script
+        - script.sql
+    - postgresql_task.rts
+
+## Python knihovny
+- re
+- sys
+- requests
+- json
+- grpc
+- unittest
+- importlib
+- threading
+- time
+- io
+- os
+
+## Příklad spuštění scriptů
+python3 file-client.py --base-url=http://127.0.0.1:5000/ --backend=rest --size=2 read 9c465aa7-05fd-46eb-b759-344c48abc85f
+python3 test_file-client.py
+
+## Aplikace
+Implementován jak rest, tak grpc. Lze rozšířit, například u restu by šlo implementovat více HTTP metod a také reakce na víc různých status kódů.
+
+## Testy
+Jednoduché unit testy. Je dobré podotknout, že unit testy pro ArgParser by se ideálně měli rozdělit pro test jednotlivých metod. 
+Nechtěl jsem se ale tolik zaměřovat na parsovaní argumentů, tak je to zjednodušené. Grpc a rest jsou testovány pouze na lokálu, cež není ideální.
+Testovací grpc server se při spuštění testů spustí paralelně, ale rest je nutné předem spustit pomocí flasku, jinak test selže.
+
+## Postgresql script
+Jednoduchý script pro vytvoření požadovaných tabulek. Další možná omezení jsou například, že by nešlo mít čas ukončení před začátkem (jak u domény, tak u flagů).
+Dalším omezením je samozřejmě, že by začátek flagu nemohl být před začátkem registrace domény a to samé pro ukončení. Také omezení překrývání flagů.
+Dále by se mohla třeba kontrolovat validita názvů a podobně.
+
+## Autor
+Jaromír Franěk (xfrane16)
--- sql/script/script.sql
+++ sql/script/script.sql
@@ -1,7 +1,111 @@
+/*
+DROP DATABASE IF EXISTS task;
+
+CREATE DATABASE task
+   WITH
+   OWNER = postgres
+   ENCODING = 'UTF8'
+   LC_COLLATE = 'en_US.UTF-8'
+   LC_CTYPE = 'en_US.UTF-8'
+   TABLESPACE = pg_default
+   CONNECTION LIMIT = -1;
+
+   \c testdatabase 
+*/
+
+DROP TABLE IF EXISTS domain, domain_flag;
+/*DROP TYPE IF EXISTS VALID_FLAGS;*/
+
+CREATE TYPE VALID_FLAGS AS ENUM ('EXPIRED', 'OUTZONE', 'DELETE_CANDIDATE');
+
+/*--Functions--*/
+CREATE OR REPLACE FUNCTION check_domain_registration(d_name varchar(1000), r_time timestamp) 
+  RETURNS boolean AS
+$$
+DECLARE
+  i timestamp;
+BEGIN
+  SELECT MAX(domain_unreg_time) INTO i FROM domain WHERE domain_name = d_name;
+  IF (i <= r_time or i IS NULL) THEN
+    RETURN true;
+  END IF;
+
+  RETURN false;  
+END
+$$ LANGUAGE plpgsql;
+
+/*little debetable, since it can be also done with exceptions (it just wont update value)*/
+CREATE OR REPLACE FUNCTION check_flag_update()
+  RETURNS trigger AS
+$BODY$
+    BEGIN
+        NEW.df_id := OLD.df_id;
+        NEW.df_domain_id := OLD.df_domain_id;
+        NEW.df_valid_flag := OLD.df_valid_flag;
+        NEW.df_reg_time := OLD.df_reg_time;
+        
+        IF OLD.df_unreg_time <> 'infinity' OR NEW.df_unreg_time < CURRENT_TIMESTAMP THEN
+            NEW.df_unreg_time := OLD.df_unreg_time;
+        END IF;
+        RETURN NEW;
+    END;
+$BODY$
+  LANGUAGE plpgsql;
+
+/*--Tables--*/
 CREATE TABLE domain (
-   domain_id    NUMERIC       NOT NULL,
-   domain_name  VARCHAR(1000) NOT NULL,
-   reg_time     DATE    NOT NULL,
-   unreg_time   DATE,
-   CONSTRAINT domain_pk PRIMARY KEY (domain_id)
+   domain_id INT GENERATED ALWAYS AS IDENTITY,
+   domain_name VARCHAR(1000),
+   domain_reg_time TIMESTAMP NOT NULL,
+   domain_unreg_time TIMESTAMP NOT NULL,
+   PRIMARY KEY(domain_id)
 );
+
+CREATE TABLE domain_flag (
+   df_id INT GENERATED ALWAYS AS IDENTITY,
+   df_domain_id INT NOT NULL,
+   df_valid_flag VALID_FLAGS,
+   df_reg_time TIMESTAMP NOT NULL,
+   df_unreg_time TIMESTAMP DEFAULT 'infinity' NOT NULL,
+   PRIMARY KEY(df_id),
+   CONSTRAINT reference_domain FOREIGN KEY(df_domain_id) REFERENCES domain(domain_id)
+);
+
+/*--Triggers and constraints--*/
+CREATE TRIGGER prevent_update BEFORE UPDATE ON domain_flag FOR EACH ROW EXECUTE PROCEDURE check_flag_update();
+
+ALTER TABLE domain ADD CONSTRAINT no_overlap CHECK (check_domain_registration(domain_name, domain_reg_time));
+
+/*Commented block with test values*/
+/*INSERT INTO domain (domain_name, domain_reg_time, domain_unreg_time) 
+VALUES('https://www.seznam.cz/', '2021-02-14 11:22:33.000', '2021-02-15 11:22:33.000');
+INSERT INTO domain (domain_name, domain_reg_time, domain_unreg_time) 
+VALUES('https://www.seznam.cz/', '2021-02-15 11:22:33.000', '2025-02-15 11:22:33.000');
+INSERT INTO domain (domain_name, domain_reg_time, domain_unreg_time) 
+VALUES('https://www.youtube.com/', '2021-02-15 10:22:33.000', '2021-02-15 11:22:33.000');
+INSERT INTO domain (domain_name, domain_reg_time, domain_unreg_time) 
+VALUES('https://www.some.cz/', '2021-02-15 11:22:33.000', '2025-02-15 11:22:33.000');
+INSERT INTO domain_flag (df_domain_id, df_valid_flag, df_reg_time, df_unreg_time) 
+VALUES(2, 'EXPIRED', '2021-02-14 11:22:33.000', '2021-02-15 11:22:33.000');
+INSERT INTO domain_flag (df_domain_id, df_valid_flag, df_reg_time) 
+VALUES(2, 'EXPIRED', '2021-02-16 11:22:33.000');
+INSERT INTO domain_flag (df_domain_id, df_valid_flag, df_reg_time) 
+VALUES(3, 'OUTZONE', '2021-02-16 11:22:33.000');
+INSERT INTO domain_flag (df_domain_id, df_valid_flag, df_reg_time) 
+VALUES(4, 'DELETE_CANDIDATE', '2021-02-16 11:22:33.000');
+INSERT INTO domain_flag (df_domain_id, df_valid_flag, df_reg_time) 
+VALUES(4, 'EXPIRED', '2025-02-16 11:22:33.000');
+INSERT INTO domain_flag (df_domain_id, df_valid_flag, df_reg_time, df_unreg_time) 
+VALUES(4, 'EXPIRED', '2019-02-16 11:22:33.000', '2020-02-16 11:22:33.000');
+
+UPDATE domain_flag SET df_unreg_time = '2023-02-16 11:22:33.000' WHERE df_id = 2;
+UPDATE domain_flag SET df_unreg_time = '2024-02-16 11:22:33.000' WHERE df_id = 2;*/
+
+/*--Selects--*/
+SELECT domain_name FROM domain LEFT OUTER JOIN domain_flag ON domain_id = df_domain_id
+WHERE domain_reg_time < CURRENT_TIMESTAMP AND domain_unreg_time > CURRENT_TIMESTAMP 
+AND (df_valid_flag IS NULL OR df_valid_flag <> 'EXPIRED' 
+OR (df_valid_flag = 'EXPIRED' AND df_reg_time > CURRENT_TIMESTAMP AND df_unreg_time < CURRENT_TIMESTAMP));
+
+SELECT domain_name FROM domain LEFT OUTER JOIN domain_flag ON domain_id = df_domain_id
+WHERE (df_valid_flag = 'EXPIRED' OR df_valid_flag = 'OUTZONE') AND df_unreg_time < CURRENT_TIMESTAMP;
